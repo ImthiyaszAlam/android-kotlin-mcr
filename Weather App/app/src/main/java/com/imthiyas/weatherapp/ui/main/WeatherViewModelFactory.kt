1dad2dff87dca54b9b248add23cd4ca3408436c2
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-08-19 23:20:03 +0530

    WeatherViewModelFactory

--- Weather App/app/src/main/java/com/imthiyas/weatherapp/ui/main/WeatherViewModelFactory.kt
+++ Weather App/app/src/main/java/com/imthiyas/weatherapp/ui/main/WeatherViewModelFactory.kt
@@ -1,8 +1,18 @@
 package com.imthiyas.weatherapp.ui.main
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.imthiyas.weatherapp.data.repository.WeatherRepository
 
 class WeatherViewModelFactory(private val repository: WeatherRepository) :
     ViewModelProvider.Factory {
+
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
+            return WeatherViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+
+    }
 }
