d8cc407bc85f08019b051e4e8b51ae18444f6578
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-09-01 22:00:18 +0530

    WeatherScreen

---
+++ Weather App/app/src/main/java/com/imthiyas/weatherapp/ui/components/WeatherScreen.kt
@@ -0,0 +1,6 @@
+package com.imthiyas.weatherapp.ui.components
+
+import com.imthiyas.weatherapp.ui.home.HomeViewModele
+
+class WeatherScreen(viewModel: HomeViewModele, city: String, apiKey: String) {
+}
